866feba7d7708a2b1996525c8643eee2aff5d007
Josh Cross <joshcross@ftscreations.com>
2021-05-20 20:34:05 -0500

    A few minor tweaks

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ const db = require('./db/sqlite');
 const searchTracks = require('./routes/searchTracks');
 const getTracks = require('./routes/getTracks');
 
-// A simple server to interface the API with a SQLite database of song information
-app.use(require('body-parser').json());
-// app.use(express.static(__dirname + '/static'));
-
 // One API endpoint which will return all songs with name and id
 // If a parameter is passed to the endpoint it will return only songs with the search term in it's title
 app.get('/tracks/:query', searchTracks);
